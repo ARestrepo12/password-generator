7bc2dbb5960d644d88d7f6283e3f697458f134b5
unknown <arestre@ncsu.edu>
2022-06-07 14:10:11 -0400

    number range working

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,16 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var passwordlength = prompt("How long would you like the password to be?");
+  const passwordlength = prompt("How long would you like the password to be?");
+  if (isNaN(passwordlength))
+  {
+    alert("Must input a number.");
+    return false;
+  }
+  if (passwordlength <8 || passwordlength >128) {
+    alert("Number must be between 8 and 128")
+    return false;
+  }
   console.log(passwordlength);
   const lowercase = confirm("Would you like lowercase letters in your password?");
   console.log(lowercase);
@@ -14,6 +23,39 @@ function generatePassword() {
   const special = confirm("Would you like special characters in your password?");
   console.log(special);
 }
+
+
+const randomFunc = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  special: randomSpecial
+};
+
+
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(randomLower());
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(randomUpper());
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(randomNumber());
+
+function randomSpecial() {
+  const symbols = "!@#$%^&*(){}[]=/,."
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(randomSpecial());
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -35,11 +77,6 @@ function writePassword() {
 
 // var passwordlength = 0;
 // passwordlength = prompt("how long would you like the password to be?");
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
