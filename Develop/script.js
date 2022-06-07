3fcba647736032dba9ed24cb0c7bb535185f2934
unknown <arestre@ncsu.edu>
2022-06-07 12:51:27 -0400

    first question works

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,21 @@
-// Assignment code here
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var passwordlength = prompt("How long would you like the password to be?");
+  console.log(passwordlength);
+  const lowercase = confirm("Would you like Lowercase letters in your password?");
+  console.log(lowercase);
+  const lowercase = confirm("Would you like Lowercase letters in your password?");
+  console.log(lowercase);
+  const lowercase = confirm("Would you like Lowercase letters in your password?");
+  console.log(lowercase);
+  const lowercase = confirm("Would you like Lowercase letters in your password?");
+  console.log(lowercase);
+  
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,6 +25,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// var passwordlength = function() {
+//  var promptlengthquestion = window.prompt('How long would you like the password to be?');
+//  if (promptlengthquestion === "" || promptlengthquestion === null) {
+//    window.alert("Please enter a number.");
+//    return passwordlength();
+//  }
+// }
+
+
+// var passwordlength = 0;
+// passwordlength = prompt("how long would you like the password to be?");
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
